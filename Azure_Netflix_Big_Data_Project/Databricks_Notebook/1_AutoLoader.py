d59c7918d3cedcf602f396e92fab73d5ede3abec
Rahul Chhagan Patil <148639048+rahul-tech03@users.noreply.github.com>
2025-06-30 14:47:32 +0530

    Update 1_AutoLoader.py

--- Azure_Netflix_Big_Data_Project/Databricks_Notebook/1_AutoLoader.py
+++ Azure_Netflix_Big_Data_Project/Databricks_Notebook/1_AutoLoader.py
@@ -9,14 +9,14 @@
 
 # COMMAND ----------
 
-checkpoint_location = "abfss://silver@rpnetflixdl.dfs.core.windows.net/checkpoint"
+checkpoint_location = "abfss://silver_container_name@storage_name.dfs.core.windows.net/checkpoint"
 
 # COMMAND ----------
 
 df = spark.readStream.format("cloudFiles") \
   .option("cloudFiles.format", "csv") \
   .option("cloudFiles.schemaLocation",checkpoint_location)\
-  .load("abfss://bronze@rpnetflixdl.dfs.core.windows.net" )
+  .load("abfss://bronze_layer@storage_account_name.dfs.core.windows.net" )
 
 # COMMAND ----------
 
@@ -28,7 +28,7 @@ df.writeStream \
   .format("delta") \
   .option("checkpointLocation", checkpoint_location) \
   .trigger(processingTime='10 seconds') \
-  .start("abfss://bronze@rpnetflixdl.dfs.core.windows.net/netflix_titles")
+  .start("abfss://bronze_layer@storage_account_name.dfs.core.windows.net/netflix_titles")
 
 
 # COMMAND ----------
